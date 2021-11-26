50e6043a5a03a693ca6fb87d383adcb08057a6d0
Dan Carlson <41755304+dw-carlson@users.noreply.github.com>
2021-11-26 15:54:04 +0900

    Add files via upload

--- paraview_particles_to_line.py
+++ paraview_particles_to_line.py
@@ -8,35 +8,56 @@ X = input0.PointData["x [mm]"]/1000
 Y = input0.PointData["y [mm]"]/1000
 Z = input0.PointData["z [mm]"]/1000
 pAge = input0.PointData["InjectionStepId"]
-coordinates = algs.make_vector(X, Y, Z)
-pts = vtk.vtkPoints()
-pts.SetData(dsa.numpyTovtkDataArray(coordinates, 'Points'))
-output.SetPoints(pts)
-output.PointData.append(index, 'Index')
-output.PointData.append(pAge, 'pAge')
-ptIds = vtk.vtkIdList()
-ptIds.SetNumberOfIds(len(X))
-for i in range(len(X)):
-   #Add the points to the line. The first value indicates
-   #the order of the point on the line. The second value
-   #is a reference to a point in a vtkPoints object. Depends
-   #on the order that Points were added to vtkPoints object.
-   #Note that this will not be associated with actual points
-   #until it is added to a vtkPolyData object which holds a
-   #vtkPoints object.
-   ptIds.SetId(i, i)
 
-difPt = 0*X
+steps=np.unique(pAge)
 
-difPt[0] = math.sqrt((X[2]-X[0])**2 + (Y[2]-Y[0])**2 + (Z[2]-Z[0])**2)
-difPt[-1] = math.sqrt((X[-1]-X[-1-2])**2 + (Y[-1]-Y[-1-2])**2 + (Z[-1]-Z[-1-2])**2)
-for i in range(1,len(X)-1):
-   difPt[i] = math.sqrt((X[i+1]-X[i-1])**2 + (Y[i+1]-Y[i-1])**2 + (Z[i+1]-Z[i-1])**2)
+xn = np.array(X)
+yn = np.array(Y)
+zn = np.array(Z)
+pn = np.array(pAge)
 
-# Allocate the number of 'cells' that will be added. We are just
-# adding one vtkPolyLine 'cell' to the vtkPolyData object.
-output.Allocate(1, 1)
-output.PointData.append(difPt, 'pyPts')
-# Add the poly line 'cell' to the vtkPolyData object.\output.PointData.append(index, 'Index')
-output.InsertNextCell(vtk.VTK_POLY_LINE, ptIds)
-print(ptIds)
+for jj in steps:
+    
+    indTmp = pn == jj
+    print(indTmp)
+    xInd = xn[indTmp]
+    yInd = yn[indTmp]
+    zInd = zn[indTmp]
+
+    coordinates = algs.make_vector(xInd, yInd, zInd)
+    pts = vtk.vtkPoints()
+    pts.SetData(dsa.numpyTovtkDataArray(coordinates, 'Points'))
+    output.SetPoints(pts)
+
+    ptIds = vtk.vtkIdList()
+    ptIds.SetNumberOfIds(len(xInd))
+      
+    for i in range(len(xInd)):
+       #Add the points to the line. The first value indicates
+       #the order of the point on the line. The second value
+       #is a reference to a point in a vtkPoints object. Depends
+       #on the order that Points were added to vtkPoints object.
+       #Note that this will not be associated with actual points
+       #until it is added to a vtkPolyData object which holds a
+       #vtkPoints object.
+       ptIds.SetId(i, i)
+       
+    
+    print(jj)
+
+    difPt = 0*xInd
+
+    difPt[0] = math.sqrt((xInd[2]-xInd[0])**2 + (yInd[2]-yInd[0])**2 + (zInd[2]-zInd[0])**2)
+    difPt[-1] = math.sqrt((xInd[-1]-xInd[-1-2])**2 + (yInd[-1]-yInd[-1-2])**2 + (zInd[-1]-zInd[-1-2])**2)
+    for i in range(1,len(xInd)-1):
+       difPt[i] = math.sqrt((xInd[i+1]-xInd[i-1])**2 + (yInd[i+1]-yInd[i-1])**2 + (zInd[i+1]-zInd[i-1])**2)
+
+    # Allocate the number of 'cells' that will be added. We are just
+    # adding one vtkPolyLine 'cell' to the vtkPolyData object.
+    output.Allocate(8,8)
+
+    output.PointData.append(difPt, 'pyPts')
+    output.PointData.append(pn[indTmp], 'pypts' + str(jj))
+
+    # Add the poly line 'cell' to the vtkPolyData object.\output.PointData.append(index, 'Index')
+    output.InsertNextCell(vtk.VTK_POLY_LINE, ptIds)
