3dc40469ae1708888b08825b74640e73c0d1d357
Dan Carlson <41755304+dw-carlson@users.noreply.github.com>
2022-03-17 12:23:29 +0900

    Add files via upload

---
+++ pvStreamTracerCurvature.py
@@ -0,0 +1,106 @@
+##################################################
+## When loaded as a programmable filter in Paraview 5.10, 
+## this script loads XYZ coordinates of streamline point data to calculate streamline curvature
+## Input: streamlines, output: rCirc = R: radius of curvature. UROUT = sqrt(2U/R)
+##################################################
+## Daniel Carlson
+## 2022 March
+## Okinawa Institute of Science and Technology
+##################################################
+
+import math
+import numpy as np
+
+from vtk.numpy_interface import algorithms as algs
+
+
+X = inputs[0].Points[:,0]
+Y = inputs[0].Points[:,1]
+Z = inputs[0].Points[:,2]
+
+
+pSource = np.array(inputs[0].PointData["InjectedPointId"])
+cohortTime = np.array(inputs[0].PointData["SeedIds"])
+
+
+
+
+UPT = np.array(inputs[0].PointData["U"])
+uPt = UPT[:,0]
+vPt = UPT[:,1]
+wPt = UPT[:,2]
+
+stepsAll, stepCounts = np.unique(pSource, return_counts = True)
+cohortSteps, stepCounts = np.unique(cohortTime, return_counts = True)
+
+xn = np.array(X)
+yn = np.array(Y)
+zn = np.array(Z)
+
+sourceID = np.array(pSource)
+cohortID = np.array(cohortTime)
+
+kk = 0
+nn = 0
+ptsLen = np.array([None for _ in range(len(stepsAll))])
+
+
+UOUT = 0*xn
+VOUT = 0*xn
+WOUT = 0*xn
+radOut = 0*xn
+kcOut = 0*xn
+curvCoord = kcOut
+rCirc = kcOut
+
+PMOUT = kcOut
+UROUT = kcOut
+
+for cohortInd in cohortSteps: 
+    
+    for jj in stepsAll:
+    
+        indTmp = np.logical_and(sourceID == jj, cohortID == cohortInd)
+  
+    
+        xInd = xn[indTmp]
+        yInd = yn[indTmp]
+        zInd = zn[indTmp]
+        
+        uInd = uPt[indTmp]
+        vInd = vPt[indTmp]
+        wInd = wPt[indTmp]
+        
+        curvTmp = 0*xInd
+        rTmp = 0*xInd+10000 #infinite radius at streamline endpoints
+        windS = 1 #delta point index for finding local curvature
+        
+        for kk in range(windS,len(xInd)-windS):
+            
+
+            A = np.array([xInd[kk-windS], yInd[kk-windS], zInd[kk-windS]])
+            B = np.array([xInd[kk], yInd[kk], zInd[kk]])
+            C = np.array([xInd[kk+windS], yInd[kk+windS], zInd[kk+windS]])
+            
+            a = np.linalg.norm(C - B)
+            b = np.linalg.norm(C - A)
+            c = np.linalg.norm(B - A)
+            
+           
+            s = (a + b + c) / 2
+            rTmp[kk] = a*b*c / 4 / np.sqrt(s * (s - a) * (s - b) * (s - c))
+      
+        rCirc[indTmp]=rTmp
+        UU = np.sqrt(uPt[indTmp]**2 + vPt[indTmp]**2 + wPt[indTmp]**2)
+
+        UOUT[indTmp]=UU
+        UROUT[indTmp]=np.sqrt(2*UU/rTmp)
+
+        
+
+output.PointData.append(rCirc, 'rCirc')
+output.PointData.append(UROUT, 'UROUT')
+
+output.PointData.append(UOUT, 'UOUT')
+
+
