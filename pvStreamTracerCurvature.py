b305a416c3a5c955ac66290b907955d94a5758dc
Dan Carlson <41755304+dw-carlson@users.noreply.github.com>
2022-04-06 17:18:04 +0900

    Add files via upload

--- pvStreamTracerCurvature.py
+++ pvStreamTracerCurvature.py
@@ -2,7 +2,7 @@
 ##################################################
 ## When loaded as a programmable filter in Paraview 5.10, 
 ## this script loads XYZ coordinates of streamline point data to calculate streamline curvature
-## Input: streamlines, output: rCirc = R: radius of curvature. UROUT = sqrt(2U/R)
+## Input: streamlines, output: rCirc = R: radius of curvature. 
 ##################################################
 ## Daniel Carlson
 ## 2022 March
@@ -12,6 +12,8 @@
 import math
 import numpy as np
 
+
+
 from vtk.numpy_interface import algorithms as algs
 
 
@@ -19,12 +21,8 @@ X = inputs[0].Points[:,0]
 Y = inputs[0].Points[:,1]
 Z = inputs[0].Points[:,2]
 
-
 pSource = np.array(inputs[0].PointData["InjectedPointId"])
-cohortTime = np.array(inputs[0].PointData["SeedIds"])
-
-
-
+cohort = np.array(inputs[0].PointData["SeedIds"])
 
 UPT = np.array(inputs[0].PointData["U"])
 uPt = UPT[:,0]
@@ -32,26 +30,27 @@ vPt = UPT[:,1]
 wPt = UPT[:,2]
 
 stepsAll, stepCounts = np.unique(pSource, return_counts = True)
-cohortSteps, stepCounts = np.unique(cohortTime, return_counts = True)
+cohortSteps, stepCounts = np.unique(cohort, return_counts = True)
 
 xn = np.array(X)
 yn = np.array(Y)
 zn = np.array(Z)
 
 sourceID = np.array(pSource)
-cohortID = np.array(cohortTime)
+cohortID = np.array(cohort)
 
 kk = 0
 nn = 0
 ptsLen = np.array([None for _ in range(len(stepsAll))])
 
+UMAGOUT = 0*xn
 UOUT = 0*xn
 VOUT = 0*xn
 WOUT = 0*xn
 radOut = 0*xn
-kcOut = 0*xn
-UROUT = 0*xn
+
 rCirc = 0*xn
+
 
 
 for cohortInd in cohortSteps: 
@@ -69,9 +68,9 @@ for cohortInd in cohortSteps:
         vInd = vPt[indTmp]
         wInd = wPt[indTmp]
         
+        defInd = defMag[indTmp]
+        FTOUT[indTmp]=FT[indTmp]
 
-        curvTmp = 0*xInd
-        strTmp = 0*xInd
         rTmp = 0*xInd+10000 #infinite radius at streamline endpoints
         windS = 1 #delta point index for finding local curvature
         
@@ -89,16 +88,24 @@ for cohortInd in cohortSteps:
            
             s = (a + b + c) / 2
             rTmp[kk] = a*b*c / 4 / np.sqrt(s * (s - a) * (s - b) * (s - c))
-
+                  
         rCirc[indTmp]=rTmp
+        
         UU = np.sqrt(uPt[indTmp]**2 + vPt[indTmp]**2 + wPt[indTmp]**2)
         
-        UOUT[indTmp]=UU
-        UROUT[indTmp]=UU/rTmp
-
+        UMAGOUT[indTmp] = UU
+        UOUT[indTmp] = uPt[indTmp]
+        VOUT[indTmp] = vPt[indTmp]
+        WOUT[indTmp] = wPt[indTmp]
+  
+  
 
 output.PointData.append(rCirc, 'rCirc')
-output.PointData.append(UROUT, 'UROUT')
+output.PointData.append(UMAGOUT, 'UMAGOUT')
+
 output.PointData.append(UOUT, 'UOUT')
+output.PointData.append(VOUT, 'VOUT')
+output.PointData.append(WOUT, 'WOUT')
+
 
 
