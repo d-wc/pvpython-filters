aeda987849a0b8d9ec9ebbad8128158dfde1f809
Dan Carlson <41755304+dw-carlson@users.noreply.github.com>
2023-02-10 11:51:02 +0900

    Update pv_strain_eigen.py

--- pv_strain_eigen.py
+++ pv_strain_eigen.py
@@ -9,7 +9,7 @@ UVECT = inputs.PointData["U"]
 STRAINTEN = 0.5*strain(UVECT)
 
 [DEIGVAL,DEIGVEC] = np.linalg.eig(STRAINTEN)
-print(DEIGVEC.shape)
+
 
 for ii in range(0,len(DEIGVAL)):
     tmpEigVal = DEIGVAL[ii,:]
@@ -22,13 +22,16 @@ for ii in range(0,len(DEIGVAL)):
     DEIGVAL[ii,:] = tmpEigVal
     DEIGVEC[ii,:,:] = tmpEigVec
 
+print(DEIGVEC.shape)
+DV0 = DEIGVEC[:,0,:]
+DV1 = DEIGVEC[:,1,:]
+DV2 = DEIGVEC[:,2,:]
 
-DV0 = DEIGVEC[0,:]
-DV1 = DEIGVEC[1,:]
-DV2 = DEIGVEC[2,:]
 
-output.PointData.append(DEIGVAL,'deigVal')
-output.PointData.append(DEIGVEC,'deigVec')
+output.PointData.append(DEIGVAL[:,0],'val0')
+output.PointData.append(DEIGVAL[:,1],'val1')
+output.PointData.append(DEIGVAL[:,2],'val2')
+
 output.PointData.append(DV0,'dv0')
 output.PointData.append(DV1,'dv1')
 output.PointData.append(DV2,'dv2')
