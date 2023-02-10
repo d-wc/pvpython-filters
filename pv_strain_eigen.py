df007e0a3c623d156ceecfd422282fb830ac7135
Dan Carlson <41755304+dw-carlson@users.noreply.github.com>
2023-02-10 10:53:55 +0900

    Update pv_strain_eigen.py

--- pv_strain_eigen.py
+++ pv_strain_eigen.py
@@ -1,16 +1,3 @@
-##################################################
-## In Paraview, apply as programmable filter to velocity field U
-## Find the strain tensor and compute eigenvectors and eigenvalues
-## output: none 
-##################################################
-## Daniel Carlson
-## 2023 February
-## Okinawa Institute of Science and Technology
-##################################################
-
-
-
-
 import numpy as np
 
 from vtk.numpy_interface import algorithms as algs
@@ -22,6 +9,26 @@ UVECT = inputs.PointData["U"]
 STRAINTEN = 0.5*strain(UVECT)
 
 [DEIGVAL,DEIGVEC] = np.linalg.eig(STRAINTEN)
+print(DEIGVEC.shape)
+
+for ii in range(0,len(DEIGVAL)):
+    tmpEigVal = DEIGVAL[ii,:]
+    tmpEigVec = DEIGVEC[ii,:,:]
+
+    idx = tmpEigVal.argsort()[::-1]
+    tmpEigVal = tmpEigVal[idx]
+    tmpEigVec = tmpEigVec[:,idx]
+
+    DEIGVAL[ii,:] = tmpEigVal
+    DEIGVEC[ii,:,:] = tmpEigVec
+
+
+DV0 = DEIGVEC[0,:]
+DV1 = DEIGVEC[1,:]
+DV2 = DEIGVEC[2,:]
 
 output.PointData.append(DEIGVAL,'deigVal')
 output.PointData.append(DEIGVEC,'deigVec')
+output.PointData.append(DV0,'dv0')
+output.PointData.append(DV1,'dv1')
+output.PointData.append(DV2,'dv2')
